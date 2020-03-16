85801fe38b29a44a023b42eeb6ff6ddac03cf414
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-16 18:16:00 -0400

    confused

--- Assets/app.js
+++ Assets/app.js
@@ -12,3 +12,33 @@ var questionDiv = document.createElement("div")
 questionDiv.setAttribute("id" , "question-div")
 
 //Create area for answers
+var answers = document.createElement("div")
+answers.setAttribute("id","answers")
+
+
+//setup
+var Quiz = {
+    //timer
+    timer:15,
+    //questions
+    questionindex:0,
+    questions: {
+        q1: "Is Javascript of the 3 languages of the web?",
+        a1: "True or false",
+        q2: "Is the color of the sky blue?",
+        a2: "True of false",
+        q3: "Is the color of the water blue?",
+        a3: "True or false",
+    }
+    init: function(){
+    questionEl.appendChild(instructions)
+    questionEl.appendChild(questions)        
+    }
+}
+
+    }
+
+
+
+
+}
