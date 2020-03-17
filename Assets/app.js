326428361d2490e8d76937effa4279abd9111a8d
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-17 19:09:32 -0400

    more tweaks, answer input not functional

--- Assets/app.js
+++ Assets/app.js
@@ -27,7 +27,7 @@ inCorrectAlert.textContent = "incorrect"
 //setup
 var quiz = {
     //timer
-    time: 15,
+    time: 10,
     //start at first question
     questionindex: 0,
     //number of correct answer
@@ -53,73 +53,94 @@ var quiz = {
 
     startQuiz: function () {
         questionEl.innerHTML = "",
+        //what question we are on
             quiz.questionindex = 0;
+            //correct score
         quiz.correct = 0;
-        quiz.time = 15;
+        //the amount of secs on our timer
+        quiz.time = 10;
+        //runs the function
         quiz.timeStart();
+        // runs the function
         quiz.nextQuestion();
 
     },
 
     nextQuestion: function () {
         if (quiz.questionindex < quiz.questions.length) {
+            // clears the question area
             questionDiv.innerHTML = "";
+            //clears the answer area
             answers.innerHTML = "";
             var currentQuestion = quiz.questions[quiz.questionindex].q;
             questionDiv.innerHTML = "<h3>" + currentQuestion + "<h3>"
             var content = document.getElementById("questions");
             content.appendChild(questionDiv);
+            // for loop for our answers
             for (var index = 0; index < quiz.questions[quiz.questionindex].a.length; index++) {
                 var currentAnswer = quiz.questions[quiz.questionindex].a[index];
                 var answerLi = document.createElement("li")
+                // will add the text to the div
                 answerLi.textContent = currentAnswer
                 answerLi.setAttribute("class", "answerli")
                 answerLi.setAttribute("data", index)
+                //will add the answers onto the page
                 answers.appendChild(answerLi)
             }
             content.appendChild(answers);
         }
         else {
+            //emptys our questions area
             questionDiv.innerHTML = "";
+            //emptys our answer area
             answers.innerHTML = "";
+            //runs the function
             quiz.timeStop();
         }
     },
     timeStart: function () {
+        //clears the timer
         clearInterval(counter);
+        //time interval is set
         counter = setInterval(quiz.countDown, 1000);
     },
 
     timeStop: function () {
+        //once the time stops it clears the counter
         clearInterval(counter)
     },
     countDown: function () {
+        //timer to rundown
         quiz.time--;
+        //show the time on the page
         timerEl.textContent = quiz.time + "seconds left";
         if (quiz.time < 1) {
-            questionDiv.innerHTML = "Out of time!"
-            quiz.time = 15;
+            //resets the time to 10
+            quiz.time = 10;
+            //moves onto the next question
             quiz.questionindex++
+            //runs the function 
             quiz.nextQuestion();
 
         }
     },
     getAnswer: function (e) {
+        //placing a conditional with event.target to search for the user input
         if (e.target.matches("li")) {
             e.target.getattribute("data");
 
             if (e.target.getattribute("data") == quiz.questions[quiz.questionindex].c) {
                 questionEl.appendChild(correctAlert);
-                quiz.time = 15;
+                quiz.time = 10;
                 quiz.questionindex++;
                 quiz.correct++;
                 quiz.nextQuestion();
             }
             else {
                 questionEl.appendChild(inCorrectAlert);
-                quiz.time = 15
-                quiz.questionindex++
-                quiz.nextQuestion()
+                quiz.time = 10;
+                quiz.questionindex++;
+                quiz.nextQuestion();
             }
         }
     }
